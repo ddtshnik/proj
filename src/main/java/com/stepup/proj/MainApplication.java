4c62452b24e5d1af075f515375b035e0c331455f
Denis Semin <ddtshnik@rambler.ru>
2024-09-03 22:31:15 +0300

    Доработка проекта 111

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hallo world!");
+        System.out.println("111111");
     }
 }
